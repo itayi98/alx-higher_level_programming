44a260231700859ce52065c81733c100aa483a40
itayi98 <106750793+itayi98@users.noreply.github.com>
2022-09-28 14:13:56 +0200

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    new = set(my_list)
+    res_list = []
     res = 0
-    for i in new:
-        res += i
-         return res
+    for item in my_list:
+        if item not in res_list:
+            res_list.append(item)
+    for uniqs in res_list:
+        res += uniqs
+    return res
